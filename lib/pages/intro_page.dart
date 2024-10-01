234e7b0b593804aac63ac7f18928ced99787809c
Cedric <claecedric@gmail.com>
2024-10-01 00:20:12 -0700

    intro page and beginning of home page

---
+++ lib/pages/intro_page.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+class IntroPage extends StatelessWidget {
+  const IntroPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Padding(
+              padding: EdgeInsets.only(bottom: 40),
+              child: Icon(Icons.search, size: 100)),
+          const Padding(
+            padding: EdgeInsets.all(20),
+            child: Text(
+              "Welcome to Addiction Beater! We're so glad you joined us, enter an addiction you would like to beat to begin:",
+              style: TextStyle(),
+              textAlign: TextAlign.center,
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 25, vertical: 20),
+            child: TextField(
+              decoration: InputDecoration(
+                  enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.black),
+                      borderRadius: BorderRadius.all(Radius.circular(20))),
+                  hintText: "Addiction..."),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 70, vertical: 10),
+            child: TextButton(
+              onPressed: () {
+                // Add your onPressed logic here
+              },
+              style: TextButton.styleFrom(
+                backgroundColor: Colors.black,
+                padding: const EdgeInsets.symmetric(
+                    horizontal: 16, vertical: 18), // Padding inside the button
+              ),
+              child: const Row(
+                mainAxisAlignment:
+                    MainAxisAlignment.center, // Center the content
+                children: [
+                  Text(
+                    'Continue',
+                    style: TextStyle(
+                        color: Colors.white, // Text color
+                        fontSize: 18, // Text size
+                        fontFamily: "Arial"),
+                  ),
+                  SizedBox(width: 8), // Space between text and icon
+                  Icon(
+                    Icons.arrow_forward, // Arrow icon
+                    color: Colors.white, // Icon color
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    ));
+  }
+}
