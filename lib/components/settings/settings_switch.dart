0e70fd30def8a7cdccc50ddcfa3211b6cdc15916
Cedric <claecedric@gmail.com>
2024-10-03 03:03:03 -0700

    beat me

---
+++ lib/components/settings/settings_switch.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+class MySettingsSwitch extends StatelessWidget {
+  final String name;
+  final bool value;
+  final ValueChanged<bool> onChanged;
+
+  const MySettingsSwitch({
+    super.key,
+    required this.name,
+    required this.value,
+    required this.onChanged,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 10),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            name,
+            style: const TextStyle(fontSize: 16),
+          ),
+          CupertinoSwitch(
+            value: value,
+            onChanged: (val) => {onChanged(val)},
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/pages/settings_page.dart
+++ lib/pages/settings_page.dart
@@ -1,5 +1,5 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:minimal_addiciton_beater/components/settings/settings_switch.dart';
 import 'package:minimal_addiciton_beater/theme/theme_provider.dart';
 import 'package:provider/provider.dart';
 
@@ -9,33 +9,19 @@ class SettingsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Settings"),
-      ),
-      body: Container(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 25, vertical: 10),
-              child: Text(
-                "Dark Mode",
-                style: TextStyle(fontSize: 16),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 10),
-              child: CupertinoSwitch(
-                value: Provider.of<ThemeProvider>(context).isDarkMode,
-                onChanged: (val) => {
-                  Provider.of<ThemeProvider>(context, listen: false)
-                      .toggleTheme()
-                },
-              ),
-            )
-          ],
+        appBar: AppBar(
+          title: const Text("Settings"),
         ),
-      ),
-    );
+        body: ListView(children: [
+          MySettingsSwitch(
+            name: "Dark Mode",
+            value: Provider.of<ThemeProvider>(context).isDarkMode,
+            onChanged: (val) {
+              Provider.of<ThemeProvider>(context, listen: false).toggleTheme();
+            },
+          ),
+          MySettingsSwitch(
+              name: "Hide Addiction", value: false, onChanged: (val) {})
+        ]));
   }
 }
