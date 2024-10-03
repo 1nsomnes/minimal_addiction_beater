f7d43c4abc66877061e3985bfeb6a0b40a1016f6
Cedric <claecedric@gmail.com>
2024-10-02 19:52:58 -0700

    dark mode fixes

--- lib/theme/dark_mode.dart
+++ lib/theme/dark_mode.dart
@@ -1,10 +1,37 @@
 import 'package:flutter/material.dart';
 
 ThemeData darkMode = ThemeData(
-    colorScheme: ColorScheme.light(
-  surface: Colors.grey.shade900,
-  primary: Colors.grey.shade600,
-  secondary: Colors.grey.shade700,
-  tertiary: Colors.grey.shade800,
-  inversePrimary: Colors.grey.shade300,
-));
+  colorScheme: ColorScheme.light(
+    surface: Colors.grey.shade900,
+    primary: Colors.grey.shade600,
+    secondary: Colors.grey.shade700,
+    tertiary: Colors.grey.shade800,
+    inversePrimary: Colors.grey.shade300,
+  ),
+  textTheme: const TextTheme(
+    displayLarge: TextStyle(color: Colors.white),
+    displayMedium: TextStyle(color: Colors.white),
+    displaySmall: TextStyle(color: Colors.white),
+    headlineLarge: TextStyle(color: Colors.white),
+    headlineMedium: TextStyle(color: Colors.white),
+    headlineSmall: TextStyle(color: Colors.white),
+    titleLarge: TextStyle(color: Colors.white),
+    titleMedium: TextStyle(color: Colors.white),
+    titleSmall: TextStyle(color: Colors.white),
+    bodyLarge: TextStyle(color: Colors.white),
+    bodyMedium: TextStyle(color: Colors.white),
+    bodySmall: TextStyle(color: Colors.white),
+    labelLarge: TextStyle(color: Colors.white),
+    labelMedium: TextStyle(color: Colors.white),
+    labelSmall: TextStyle(color: Colors.white),
+  ),
+  appBarTheme: const AppBarTheme(
+    titleTextStyle: TextStyle(
+      color: Colors.white,
+      fontSize: 25,
+    ),
+    iconTheme: IconThemeData(
+      color: Colors.white,
+    ),
+  ),
+);
